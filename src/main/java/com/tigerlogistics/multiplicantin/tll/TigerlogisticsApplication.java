f75d7295ba94b5cddd50d9f3a48c182456477964
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2021-11-22 11:30:08 +0530

    introductory files added

---
+++ src/main/java/com/tigerlogistics/multiplicantin/tll/TigerlogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.multiplicantin.tll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerlogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerlogisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tigerlogistics/multiplicantin/tll/TigerlogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tigerlogistics.multiplicantin.tll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerlogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
